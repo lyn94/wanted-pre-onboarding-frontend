1eae419889202f02447e4ea268892d175af84707
lyn94 <lyn_94@naver.com>
2023-08-02 15:29:32 +0900

    chore: 페이지 이름 수정

--- src/lib/client/routerPaths.js
+++ src/lib/client/routerPaths.js
@@ -2,7 +2,7 @@ const RouterPaths = {
   home: { path: "/", name: "Home" },
   todo: { path: "/todo", name: "Todo" },
   signin: { path: "/signin", name: "Sign In" },
-  signup: { path: "/signup", name: "Join Us" },
+  signup: { path: "/signup", name: "Sign Up" },
 };
 
 export default RouterPaths;
