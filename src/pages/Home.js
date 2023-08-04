b43f36323c0245def208d92726f7b0495875b33e
lyn94 <lyn_94@naver.com>
2023-08-04 12:56:18 +0900

    feat: 메인홈 컴포넌트 정리

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div>안녕하세요, 원티드 프론트엔드 인턴십 지원자 이유나 입니다.</div>
+      <div>Todo List는 로그인 후 이용할 수 있습니다.</div>
+    </div>
+  )
+  
 };
 
 export default Home;
