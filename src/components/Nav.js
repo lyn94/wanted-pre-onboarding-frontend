ecf8ed8bce34f5e68eeaa39bcb1d156468e41f8b
lyn94 <lyn_94@naver.com>
2023-08-04 12:55:36 +0900

    refactor: classname 병합 util 모듈 추가

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,23 +1,29 @@
 import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import RouterPaths from "../lib/client/routerPaths";
+import { cls } from '../lib/common/utils';
 
 const Nav = () => {
   const location = useLocation();
-  return(
-    <div className="flex justify-between flex-row w-full">
-    {
-      Object.keys(RouterPaths).map((urlKey) => {
+  return (
+    <div className="flex justify-around flex-row w-full my-6 mx-auto">
+      {Object.keys(RouterPaths).map((urlKey) => {
         const url = RouterPaths[urlKey];
-        return(
-          <NavLink to={url.path} key={url.name} className={location.pathname === url.path && "text-red-500"} >{url.name}</NavLink>
-        )
-      })
-    }
+        return (
+          <NavLink
+            to={url.path}
+            key={url.name}
+            className={cls(
+              "p-4 rounded-[50%]",
+              location.pathname === url.path && "text-black font-bold bg-orange-200"
+            )}
+          >
+            {url.name}
+          </NavLink>
+        );
+      })}
     </div>
-
-  )
-  
+  );
 };
 
 export default Nav;
